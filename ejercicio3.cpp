9e3d7727eb488058b7106c1a0a0f4af8cc3c3da2
davento <daniela.vento@utec.edu.pe>
2019-08-22 08:55:51 -0500

    Ejercicio modificado en clase

--- ejercicio3.cpp
+++ ejercicio3.cpp
@@ -2,7 +2,9 @@
 using std::cout;
 
 int main() {
-    for (int i = 0; i < ; i++) {
+    for (int i = 0; i < 10; i++) {
+        cout<<i<<" ";
         //imprimir los 10 numeros [0-9]
     }
+    cout<<"\n";
 }
