4ba7a7b72ac4fd4818a5b25685224642915e5ace
davento <daniela.vento@utec.edu.pe>
2019-08-22 16:38:49 -0500

    Error corregido.

--- ejercicio5.cpp
+++ ejercicio5.cpp
@@ -1,12 +1,25 @@
 #include <iostream>
-using std::cout, std::vector;
+
+#include <vector>
+
+using std::cout;
+
+using std::vector;
+
 
 int main() {
 
+
     vector<int> a {1, 2, 3, 4, 5};
+
     for (auto i : a) {
+
         cout<<i<<" ";
+
     }
+
     cout<<"\n";
+
     return 0;
+
 }
